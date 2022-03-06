91490ee872f102e753c09269af706fe95988a0bc
OKURA Masafumi <masafumi.o1988@gmail.com>
2022-03-06 23:34:50 +0900

    Initial commit

---
+++ lib/feast.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "feast/version"
+
+module Feast
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/feast/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Feast
+  VERSION = "0.1.0"
+end
